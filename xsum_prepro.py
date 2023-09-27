fa26019c665551a8c263868e06cb92968736072c
unknown <mari970@fasoo.com>
2023-09-27 13:04:49 +0900

    Add: xsum_prepro.py

--- xsum_prepro.py
+++ xsum_prepro.py
@@ -1,8 +1,14 @@
 # from datasets import load_dataset
 # dataset = load_dataset("xsum")
 
-import tarfile
+# Dataset unzip
+# import tarfile
+# file = tarfile.open('XSUM-EMNLP18-Summary-Data-Original.tar.gz')
+# file.extractall('./XSUMdata')
+# file.close()
 
-file = tarfile.open('XSUM-EMNLP18-Summary-Data-Original.tar.gz')
-file.extractall('./XSUMdata')
-file.close()
+# Prompting
+import os
+file_path = 'XSUMdata/bbc-summary-data/'
+file_list = []
+with open(file_name, 'r') as f:
