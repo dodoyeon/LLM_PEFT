687eaf9884aa7b94695010e1d4d44c6c86c9e856
unknown <mari970@fasoo.com>
2023-10-30 09:52:13 +0900

    Add(gen): generate test sent

--- hugprefix_gen.py
+++ hugprefix_gen.py
@@ -66,16 +66,21 @@ def draw_graph(dir):
 
 
 def test_gen(model, dataset, tokenizer, args):
+    gen_dir = os.path.join(args.output_dir, 'generate.txt')
+    with open(gen_dir, 'w') as f:
+        f.write(f'<Generated Output>\n')
     test_loader = DataLoader(dataset, pin_memory=True)
-    for step, inputs in enumerate(test_loader):
+    for step, inputs in enumerate(tqdm(test_loader)):
         input_ids = tokenizer.encode(inputs['inputs_pretokenized'][0], return_tensors='pt')
         outputs = model.generate(input_ids=input_ids, max_new_tokens=20)
         if step % 1000 == 0:
-            print(f'[{step}th Generated Output]')
-            print(inputs['inputs_pretokenized'][0])
-            print(inputs['targets_pretokenized'][0])
-            print(tokenizer.decode(outputs[0,:].tolist()))
-    print()
+            with open(gen_dir, 'a') as f:
+                f.write(f'[{step}th Generated Output]\n')
+                f.write(f'{inputs["inputs_pretokenized"][0]}\n')
+                f.write(f'{inputs["targets_pretokenized"][0]}\n')
+                f.write(f'{tokenizer.decode(outputs[0,:].tolist())}\n')
+                f.write('\n')
+    print('Done')
 
 def main():
     parser = argparse.ArgumentParser()
@@ -88,21 +93,21 @@ def main():
     # parser.add_argument()
     args = parser.parse_args()
 
-    # model_chkpt = os.path.join(args.output_dir, 'model.pt')
-    # # model = AutoModelForCausalLM.from_pretrained(model_chkpt) # 일반 AutoModel 로는 PEFT 로 학습한 모델을 불러올 수 없다. (당연함. Adapter 같은애들은 새로운 모듈을 추가.)
-    # model = AutoPeftModelForCausalLM.from_pretrained(model_chkpt)
+    model_chkpt = os.path.join(args.output_dir, 'model.pt')
+    # model = AutoModelForCausalLM.from_pretrained(model_chkpt) # 일반 AutoModel 로는 PEFT 로 학습한 모델을 불러올 수 없다. (당연함. Adapter 같은애들은 새로운 모듈을 추가.)
+    model = AutoPeftModelForCausalLM.from_pretrained(model_chkpt)
 
-    # tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path, pad_token='<pad>')
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path, pad_token='<pad>')
 
-    # dataset = load_dataset("bigscience/P3", name="xsum_summarize_this_DOC_summary")['test']
-    # # dataset_te = DatasetDict({test: dataset['test']})
+    dataset = load_dataset("bigscience/P3", name="xsum_summarize_this_DOC_summary")['test']
+    # dataset_te = DatasetDict({test: dataset['test']})
             
-    # dataset = dataset.remove_columns(['inputs', 'targets'])
+    dataset = dataset.remove_columns(['inputs', 'targets'])
     
-    # # test_gen(model, dataset, tokenizer, args)
+    test_gen(model, dataset, tokenizer, args)
 
-    os.environ['KMP_DUPLICATE_LIB_OK']='True'
-    draw_graph('C:/Users/user/Downloads/output_20231019_090057/output_20231019_090057') # args.output_dir
+    # os.environ['KMP_DUPLICATE_LIB_OK']='True'
+    # draw_graph(args.output_dir) 
 
 
 if __name__ == '__main__':
