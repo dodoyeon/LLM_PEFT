f29e0ad356994b2c92c72d10144e2c9a88da96d3
unknown <mari970@fasoo.com>
2023-12-14 17:42:53 +0900

    Modify(hug_gen)

--- hug_gen.py
+++ hug_gen.py
@@ -13,55 +13,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def draw_graph(dir):
-    tr = os.path.join(dir, 'tr_result.txt')
-    te = os.path.join(dir, 'te_result.txt')
-    trloss, trppl, teloss, teppl = [], [], [], []
-    with open(tr, 'r') as f:
-        l = f.readlines()
-        for line in l[1:]:
-            a, b = line.split(',')
-            trloss.append(float(a))
-            trppl.append(float(b.strip()))
-
-    with open(te, 'r') as f:
-        l = f.readlines()
-        for line in l[1:]:
-            a, b = line.split(',')
-            teloss.append(float(a))
-            teppl.append(float(b))
-
-    epoch = list(range(1, 11, 1))
-    epoch_step = list(np.arange((1/3), (31/3), (1/3)))
-
-    fig, ax = plt.subplots(2, 2)
-    # Train Loss
-    ax[0, 0].plot(epoch, trloss, color = 'b', marker='o', markersize=3)
-    ax[0, 0].set_title('Train Loss')
-    ax[0, 0].set_xlabel('epochs')
-    ax[0, 0].set_ylabel('loss')
-    # ax[0, 0].set_ylim([0.9, 1.2])
-    ax[0, 0].grid(True)
-
-    # Train ppl
-    ax[0, 1].plot(epoch, trppl, color = 'g', marker='o', markersize=3)
-    ax[0, 1].set_title('Train PPL')
-    ax[0, 1].set_ylabel('ppl')
-    ax[0, 1].grid(True)
-
-    # Eval Loss
-    ax[1, 0].plot(epoch_step, teloss, color = 'b', marker='o', markersize=3)
-    ax[1, 0].set_title('Eval Loss')
-    ax[1, 0].set_xticks(list(range(0, 11)))
-    ax[1, 0].grid(True)
-
-    # Eval ppl
-    ax[1, 1].plot(epoch_step, teppl, color = 'g', marker='o', markersize=3)
-    ax[1, 1].set_title('Eval PPL')
-    ax[1, 1].set_xticks(list(range(0, 11)))
-    ax[1, 1].grid(True)
-
-    fig.tight_layout()
-    plt.show()
     plt.savefig('graph')
 
 
@@ -93,7 +44,7 @@ def test_gen(model, dataset, tokenizer, device, args):
                     f.write(f"{tokenizer.decode(outputs[0,:].tolist())}\n\n")
                     f.write('\n')
 
-    elif args.data_choice == 'org_xsum':
+    elif args.data_choice == 'xsum':
         for step, inputs in enumerate(tqdm(test_loader)):
             input_ids = tokenizer.encode(inputs['document'][0], return_tensors='pt')
             outputs = model.generate(input_ids=input_ids.to(device), max_new_tokens=10) # return_full_text=False
@@ -110,33 +61,6 @@ def test_gen(model, dataset, tokenizer, device, args):
         print('Done')
 
 
-def debug_gen(model, dataset, tokenizer, device, args):
-    model = model.to(device)
-    test_loader = DataLoader(dataset, pin_memory=True)
-    gen_dir = os.path.join(args.output_dir, 'generate.txt')
-    with open(gen_dir, 'w') as f:
-        f.write(f'<Generated Output>\n\n')
-    for step, inputs in enumerate(tqdm(test_loader)):
-        input_ids = tokenizer.encode(inputs['inputs_pretokenized'][0], return_tensors='pt')
-        outputs = model.generate(input_ids=input_ids.to(device),
-                                    max_length=512,
-                                    do_sample=True,
-                                    repetition_penalty=0.5,
-                                    eos_token_id= tokenizer.eos_token_id,
-                                    bos_token_id=tokenizer.bos_token_id,
-                                    use_cache=True
-                                    )
-        print(step)
-        with open(gen_dir, 'a',encoding='UTF-8') as f:
-            f.write(f"[{step}th Generated Output]\n")
-            inp = inputs['inputs_pretokenized'][0]# .encode('utf8')
-            f.write(f"{inp}\n\n")
-            tar = inputs['targets_pretokenized'][0]# .encode('utf8')
-            f.write(f"{tar}\n\n")
-            f.write(f"{tokenizer.decode(outputs[0,:].tolist())}\n\n")
-            f.write('\n')
-    print('Done')
-
 def use_instructed():
     dataset = load_dataset("bigscience/P3", name="xsum_summarize_this_DOC_summary")['test']
     dataset = dataset.remove_columns(['inputs', 'targets'])
@@ -146,8 +70,9 @@ def use_instructed():
 # To use same index of original and instructed(p3) xsum dataset
 def make_instructed():
     if args.data_choice == 'p3':
+        pass
 
-    elif args.data_choice == 'org_xsum':
+    elif args.data_choice == 'xsum':
         # Prompt tuning
         dataset = load_dataset("EdinburghNLP/xsum")['test']
         dataset = dataset.map(
@@ -165,13 +90,13 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_name_or_path', default= 'gpt2-large',
                         dest ='model_name_or_path', help='base model')
-    parser.add_argument('--output_dir', default=r'C:\Users\user\Downloads\output_20231019_090057\output_20231019_090057',
+    parser.add_argument('--output_dir', default='.',
                         help='experiment result save directory')  #  output_pt_20231102_004015 , output_20231019_090057
     parser.add_argument('--max_length', '-ml', default=984, type=int,
                         dest='max_length', help='maximum sequence length')
     parser.add_argument('--seed', type=int, default=42)
-    parser.add_argument('--met_choice', choices=['peft', 'original'], default='peft')
-    parser.add_argument('--data_choice', choices=['p3', 'org_xsum'], default='p3')
+    parser.add_argument('--met_choice', default='original', choices=['peft', 'original'])
+    parser.add_argument('--data_choice', default='xsum', choices=['p3', 'xsum'])
     parser.add_argument('--debug', default=True)
     args = parser.parse_args()
 
@@ -206,6 +131,13 @@ def main():
     else:
         raise NotImplementedError
     
+    dataset = load_dataset("bigscience/P3", name="xsum_summarize_this_DOC_summary")['test']
+    idx_list = list(range(0, len(dataset)))
+    num_train_idxs = random.sample(idx_list, 100)
+
+    dataset = Subset(dataset, num_train_idxs)
+    print('  - Dataset sampling randomly for generation')
+    
     tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path, pad_token='<pad>')
     
     # Prefix tuning : p3-xsum 데이터셋 (사실 이거말고 일반 xsum 데이터셋을 쓰는게 맞지만 이렇게 학습시켜버려서,,)
@@ -213,11 +145,6 @@ def main():
     dataset = make_instructed()
     
     if args.debug:
-        idx_list = list(range(0, len(dataset)))
-        num_train_idxs = random.sample(idx_list, 100)
-
-        dataset = Subset(dataset, num_train_idxs)
-        print('done')
         debug_gen(model, dataset, tokenizer, device, args)
 
     else:
