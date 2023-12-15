1a966dcc42a254b688d4fa035e2a71b7871210e4
dodoyeon <mari970@naver.com>
2023-12-15 09:51:41 +0900

    Modify(hug_gen):multi model(pret,prot,ia3) compare test

--- hug_gen.py
+++ hug_gen.py
@@ -13,10 +13,136 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def draw_graph(dir):
+    tr = os.path.join(dir, 'tr_result.txt')
+    te = os.path.join(dir, 'te_result.txt')
+    trloss, trppl, teloss, teppl = [], [], [], []
+    with open(tr, 'r') as f:
+        l = f.readlines()
+        for line in l[1:]:
+            a, b = line.split(',')
+            trloss.append(float(a))
+            trppl.append(float(b.strip()))
+
+    with open(te, 'r') as f:
+        l = f.readlines()
+        for line in l[1:]:
+            a, b = line.split(',')
+            teloss.append(float(a))
+            teppl.append(float(b))
+
+    epoch = list(range(1, 11, 1))
+    epoch_step = list(range(1, 31, 1))
+
+    fig, ax = plt.subplots(2, 2)
+    ax[0, 0].plot(epoch, trloss, color = 'b', marker='o', markersize=3)
+    ax[0, 0].set_title('Train Loss')
+    ax[0, 0].set_xlabel('epochs')
+    ax[0, 0].set_ylabel('loss')
+    # ax[0, 0].set_ylim([0.9, 1.2])
+    ax[0, 0].grid(True)
+
+    ax[0, 1].plot(epoch, trppl, color = 'g', marker='o', markersize=3)
+    ax[0, 1].set_title('Train PPL')
+    ax[0, 1].grid(True)
+
+    ax[1, 0].plot(epoch_step, teloss, color = 'b', marker='o', markersize=3)
+    ax[1, 0].set_title('Eval Loss')
+    # ax[1, 0].set_xticks([0.9, 1.0])
+    ax[1, 0].set_xticks([1, 10])
+    ax[1, 0].grid(True)
+
+    ax[1, 1].plot(epoch_step, teppl, color = 'g', marker='o', markersize=3)
+    ax[1, 1].set_title('Eval PPL')
+    ax[1, 1].grid(True)
+
+    fig.tight_layout()
+    plt.show()
     plt.savefig('graph')
 
 
-def test_gen(model, dataset, tokenizer, device, args):
+"""
+The index of original and instructed(p3) xsum dataset not same for prompt, prefix, ia3 PEFT.
+To solve, add instruction 'Summarize this document:' to original xsum dataset.
+Prompt and Prefix tuning dont need text instruction(prompt) they use instead tensor prompt!
+"""
+def make_instructed(dataset, args):
+    if args.data_choice == 'p3':
+        dataset = dataset.remove_columns(['inputs', 'targets'])
+        print('  - Using dataset p3 xsum instructed dataset')
+
+    elif args.data_choice == 'xsum':
+        def preprocess_func(examples):
+            inst= "Summarize this document: "
+            sep= "\nSummary: "
+            inputs = examples['document']
+            examples['document'] = inst+inputs+sep
+            # return examples -> 필요 없을 거 같다
+
+        dataset = dataset.map(
+            preprocess_func,
+            batched=True,
+            num_proc=args.num_proc,
+            remove_columns=dataset.dataset.column_names 
+        )
+
+        print("  -Making original xsum dataset same with P3 xsum by adding 'Summarize this document:' instruction, 'Summary: ' separate token(?)")
+    return dataset
+
+def set_single_model(args):
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path, pad_token='<pad>')
+
+    if args.met_choice == 'original':
+        # 일반 AutoModel 로는 PEFT 로 학습한 모델을 불러올 수 없다. (당연함. Adapter 같은애들은 새로운 모듈을 추가.)
+        model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path)
+        print('original trained model')
+
+    elif args.met_choice == 'peft':
+        model_chkpt = os.path.join(args.output_dir, 'model.pt')
+        model = AutoPeftModelForCausalLM.from_pretrained(model_chkpt)
+        print(f'peft trained model {args.output_dir}')
+
+    else:
+        raise NotImplementedError
+    
+    if args.data_choice == 'p3':
+        dataset = load_dataset("bigscience/P3", name="xsum_summarize_this_DOC_summary")['test']
+    elif args.data_choice == 'xsum':
+        dataset = load_dataset('EdinburghNLP/xsum')['test']
+
+    idx_list = list(range(0, len(dataset)))
+    num_train_idxs = random.sample(idx_list, 100)
+    dataset = Subset(dataset, num_train_idxs)
+    print('  - Dataset sampling randomly for generation')
+
+    return dataset, model, tokenizer
+
+
+def set_multi_model(args):
+    dataset = load_dataset('EdinburghNLP/xsum')['test']
+    
+    idx_list = list(range(0, len(dataset)))
+    num_train_idxs = random.sample(idx_list, 100)
+    dataset = Subset(dataset, num_train_idxs)
+    print('  - Dataset sampling randomly for generation')
+
+    # IA3 는 instruct 를 추가해줘야한다.
+    dataset2 = make_instructed(dataset, args)
+
+    # Prefix tuning model
+    model_chkpt = ''
+    model1 = AutoPeftModelForCausalLM.from_pretrained(model_chkpt)
+
+    # Prompt tuning model
+    model_chkpt = ''
+    model2 = AutoPeftModelForCausalLM.from_pretrained(model_chkpt)
+
+    # (IA)3 model
+    model_chkpt = ''
+    model3 = AutoPeftModelForCausalLM.from_pretrained(model_chkpt)
+
+    return dataset, dataset2, model1, model2, model3 
+
+def test_single(model, dataset, tokenizer, device, args):
     model = model.to(device)
     test_loader = DataLoader(dataset, pin_memory=True)
     gen_dir = os.path.join(args.output_dir, 'generate.txt')
@@ -60,40 +186,8 @@ def test_gen(model, dataset, tokenizer, device, args):
                     f.write('\n')
         print('Done')
 
-
-def use_instructed():
-    dataset = load_dataset("bigscience/P3", name="xsum_summarize_this_DOC_summary")['test']
-    dataset = dataset.remove_columns(['inputs', 'targets'])
-    print('  -Dataset p3 xsum instructed dataset')
-    return dataset
-
-
-"""
-The index of original and instructed(p3) xsum dataset not same for prompt, prefix, ia3 PEFT.
-To solve, add instruction 'Summarize this document:' to original xsum dataset.
-Prompt and Prefix tuning dont need text instruction(prompt) they use instead tensor prompt!
-"""
-
-def make_instructed(args):
-    if args.data_choice == 'p3':
-        pass
-
-    elif args.data_choice == 'xsum':
-        def preprocess_func(examples):
-            sep1= "Summarize this document: "
-            sep2= "\nSummary: "
-            examples[]
-
-        dataset = dataset.map(
-            preprocess_func,
-            batched=True,
-            num_proc=args.num_proc,
-            remove_columns=['id'] 
-        )
-
-    print("  -Making original xsum dataset same with P3 xsum by adding 'Summarize this document:' instruction, 'Summary: ' separate token(?)")
-    return dataset
-
+def test_multi(dataset, dataset2, model1, model2, model3, tokenizer):
+    pass
 
 def main():
     parser = argparse.ArgumentParser()
@@ -104,7 +198,7 @@ def main():
     parser.add_argument('--max_length', '-ml', default=984, type=int,
                         dest='max_length', help='maximum sequence length')
     parser.add_argument('--seed', type=int, default=42)
-    parser.add_argument('--met_choice', default='original', choices=['peft', 'original'])
+    parser.add_argument('--met_choice', default='multi', choices=['peft', 'original', 'multi'])
     parser.add_argument('--data_choice', default='xsum', choices=['p3', 'xsum'])
     parser.add_argument('--debug', default=True)
     parser.add_argument('--num_proc', default=16, help='The number of process of mapping preprocess')
@@ -128,38 +222,14 @@ def main():
     # device = torch.device('cpu')
     print('Device: ', device)
 
-    if args.met_choice == 'original':
-        # 일반 AutoModel 로는 PEFT 로 학습한 모델을 불러올 수 없다. (당연함. Adapter 같은애들은 새로운 모듈을 추가.)
-        model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path)
-        print('original trained model')
-
-    elif args.met_choice == 'peft':
-        model_chkpt = os.path.join(args.output_dir, 'model.pt')
-        model = AutoPeftModelForCausalLM.from_pretrained(model_chkpt)
-        print(f'peft trained model {args.output_dir}')
+    if args.met_choice == 'multi':
+        dataset, dataset2, model1, model2, model3, tokenizer = set_multi_model(args)
+        test_multi(dataset, dataset2, model1, model2, model3, tokenizer)
 
     else:
-        raise NotImplementedError
-    
-    dataset = load_dataset("bigscience/P3", name="xsum_summarize_this_DOC_summary")['test']
-    idx_list = list(range(0, len(dataset)))
-    num_train_idxs = random.sample(idx_list, 100)
-
-    dataset = Subset(dataset, num_train_idxs)
-    print('  - Dataset sampling randomly for generation')
-    
-    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path, pad_token='<pad>')
-    
-    # Prefix tuning : p3-xsum 데이터셋 (사실 이거말고 일반 xsum 데이터셋을 쓰는게 맞지만 이렇게 학습시켜버려서,,)
-    # use_instructed()
-    dataset = make_instructed()
-    
-    if args.debug:
-        debug_gen(model, dataset, tokenizer, device, args)
-
-    else:
-        # for evaluation metric like ROUGE, etc. [Not Implemented]
-        test_gen(model, dataset, tokenizer, device, args)
+        dataset, model, tokenizer = set_single_model(args)
+        test_single(model, dataset, tokenizer, device, args)
+        
 
     # os.environ['KMP_DUPLICATE_LIB_OK']='True'
     # draw_graph(args.output_dir)
