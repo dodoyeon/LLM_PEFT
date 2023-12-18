6a319cacacba027e01eb4b32f08cfa415ede4912
dodoyeon <mari970@naver.com>
2023-12-18 10:11:35 +0900

    Modify(hug_gen):add save json

--- hug_gen.py
+++ hug_gen.py
@@ -59,6 +59,33 @@ def draw_graph(dir):
     plt.show()
     plt.savefig('graph')
 
+def save_txt(args):
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+
+    gen_dir = os.path.join(args.output_dir, 'generate.txt')
+    with open(gen_dir, 'w') as f:
+        f.write(f'<Generated Output>\n\n')
+
+    with open(gen_dir, 'a',encoding='UTF-8') as f:
+        f.write(f"[{step}th Generated Output]\n")
+        inp = inputs['document'][0]
+        f.write(f"{inp}\n\n")
+        tar = inputs['summary'][0]
+        f.write(f"{tar}\n\n")
+        f.write(f"{tokenizer.decode(outputs[0,:].tolist())}\n\n")
+        f.write('\n')
+    print("  - Save generated outputs with text file .txt")
+
+def save_json(args, out_dict_list):
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
+    
+    gen_dir = os.path.join(args.output_dir, 'generate.json')
+    with open(gen_dir, 'w') as f:
+        json.dump(out_dict_list, f)
+
+    print("  - Save generated outputs with json file .json")
 
 """
 The index of original and instructed(p3) xsum dataset not same for prompt, prefix, ia3 PEFT.
@@ -116,7 +143,7 @@ def set_single_model(args):
     return dataset, model, tokenizer
 
 
-def set_multi_model(args):
+def set_multi(args):
     dataset = load_dataset('EdinburghNLP/xsum')['test']
     # IA3 는 instruct 를 추가해줘야한다.
     dataset2 = make_instructed(dataset, args)
@@ -126,17 +153,7 @@ def set_multi_model(args):
     dataset = Subset(dataset, num_train_idxs)
     print('  - Dataset sampling randomly for generation')
 
-    # Prefix tuning model
-    model_chkpt = os.path.join('C:/Users/mari970/Desktop/pret_concat/output_20231019_090057', 'model.pt')
-    model1 = AutoPeftModelForCausalLM.from_pretrained(model_chkpt)
-
-    # Prompt tuning model
-    model_chkpt = os.path.join('C:/Users/mari970/Desktop/prot_concat/output_pt', 'model.pt')
-    model2 = AutoPeftModelForCausalLM.from_pretrained(model_chkpt)
-
-    tokenizer = AutoTokenizer.from_pretrained(model_chkpt)
-
-    return dataset, dataset2, model1, model2, tokenizer
+    return dataset, dataset2
 
 def test_single(model, dataset, tokenizer, device, args):
     model = model.to(device)
@@ -182,15 +199,19 @@ def test_single(model, dataset, tokenizer, device, args):
                     f.write('\n')
         print('Done')
 
-def test_multi(dataset, dataset2, model1, model2, tokenizer, device, args):
+def test_multi(dataset, dataset2, device, args):
     xsum_loader = DataLoader(dataset, pin_memory=True)
     p3_loader = DataLoader(dataset2, pin_memory=True)
     out_dict_list = []
-    gen_dir = os.path.join(args.output_dir, 'generate.txt')
-    with open(gen_dir, 'w') as f:
-        f.write(f'<Generated Output>\n\n')
     
     # 3개 모델에 대해서 generate 를 돌려야 하는데 3번 for 문을 돌리는 것이 효율적인지 아니면 한번에 2개 모델에 넣어서 생성을 만드는 것이 나은지
+    # -> 생각하기 귀찮으니까 그냥 따로따로 하자..
+
+    # Prefix tuning model
+    model_chkpt = os.path.join('C:/Users/mari970/Desktop/pret_concat/output_20231019_090057', 'model.pt')
+    model = AutoPeftModelForCausalLM.from_pretrained(model_chkpt)
+    tokenizer = AutoTokenizer.from_pretrained(model_chkpt)
+
     for step, inputs in enumerate(tqdm(xsum_loader)):
         input_ids = tokenizer.encode(inputs['inputs_pretokenized'][0], return_tensors='pt')
         outputs = model.generate(input_ids=input_ids.to(device),
@@ -201,31 +222,44 @@ def test_multi(dataset, dataset2, model1, model2, tokenizer, device, args):
                                     bos_token_id=tokenizer.bos_token_id,
                                     use_cache=True
                                     )
-        out_dict = {'Article':inputs, 'pret_out': pret_out, 'prot_out':prot_out}
+        out_dict = {'Article':inputs, 'pret_out': outputs}
         out_dict_list.append(out_dict)
+
+    # Prompt tuning model
+    model_chkpt = os.path.join('C:/Users/mari970/Desktop/prot_concat/output_pt', 'model.pt')
+    model = AutoPeftModelForCausalLM.from_pretrained(model_chkpt)
+    tokenizer = AutoTokenizer.from_pretrained(model_chkpt)
+    
+    for step, inputs in enumerate(tqdm(xsum_loader)):
+        input_ids = tokenizer.encode(inputs['inputs_pretokenized'][0], return_tensors='pt')
+        outputs = model.generate(input_ids=input_ids.to(device),
+                                    max_length=512,
+                                    do_sample=True,
+                                    repetition_penalty=0.5,
+                                    eos_token_id= tokenizer.eos_token_id,
+                                    bos_token_id=tokenizer.bos_token_id,
+                                    use_cache=True
+                                    )
+        out_dict_list[step]['prot_out'] = outputs
 
     # (IA)3 model
     model_chkpt = r'C:\Users\mari970\Downloads\dyoen\dyoen\LLM_PEFT\output_pt_20231210_163501_ia3\checkpoint-119028'
     model = AutoPeftModelForCausalLM.from_pretrained(model_chkpt)
+    tokenizer = AutoTokenizer.from_pretrained(model_chkpt)
 
     for step, inputs in enumerate(tqdm(p3_loader)):
         input_ids = tokenizer.encode(inputs['document'][0], return_tensors='pt')
         outputs = model.generate(input_ids=input_ids.to(device), max_new_tokens=10) # return_full_text=False
 
-    with open(gen_dir, 'a',encoding='UTF-8') as f:
-        f.write(f"[{step}th Generated Output]\n")
-        inp = inputs['document'][0]
-        f.write(f"{inp}\n\n")
-        tar = inputs['summary'][0]
-        f.write(f"{tar}\n\n")
-        f.write(f"{tokenizer.decode(outputs[0,:].tolist())}\n\n")
-        f.write('\n')
+        out_dict_list[step]['ia3_out'] = outputs
+
+    save_json(args, out_dict_list)
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_name_or_path', default= 'gpt2-large',
                         dest ='model_name_or_path', help='base model')
-    parser.add_argument('--output_dir', default='.',
+    parser.add_argument('--output_dir', default='./generated_output',
                         help='experiment result save directory')  #  output_pt_20231102_004015 , output_20231019_090057
     parser.add_argument('--max_length', '-ml', default=984, type=int,
                         dest='max_length', help='maximum sequence length')
@@ -247,15 +281,12 @@ def main():
         # torch.cuda.manual_seed_all(args.seed)  # add
         # torch.set_deterministic(True)
 
-    if not os.path.exists(args.output_dir):
-        os.makedirs(args.output_dir)
-
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print('Device: ', device)
 
     if args.met_choice == 'multi':
-        dataset, dataset2, model1, model2, tokenizer = set_multi_model(args)
-        test_multi(dataset, dataset2, model1, model2, tokenizer, device)
+        dataset, dataset2 = set_multi(args)
+        test_multi(dataset, dataset2, device)
 
     else:
         dataset, model, tokenizer = set_single_model(args)
